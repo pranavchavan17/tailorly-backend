267837375c5a97c56197bcd204a79985f8a02aad
pranavchavan17 <pranavc671@gmail.com>
2026-06-19 10:27:32 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/tailorly/tailorly_backend/TailorlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tailorly.tailorly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailorlyBackendApplication.class, args);
+	}
+
+}
