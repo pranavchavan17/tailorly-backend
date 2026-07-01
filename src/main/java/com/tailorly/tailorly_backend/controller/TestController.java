ba117125dfe9c16c6cad9a97bec2d0979eb4b5ff
pranavchavan17 <pranavc671@gmail.com>
2026-07-01 08:37:50 +0530

    Configure MongoDB Atlas and verify CRUD operations

--- src/main/java/com/tailorly/tailorly_backend/controller/TestController.java
+++ src/main/java/com/tailorly/tailorly_backend/controller/TestController.java
@@ -1,13 +1,33 @@
 package com.tailorly.tailorly_backend.controller;
 
+import com.tailorly.tailorly_backend.model.User;
+import com.tailorly.tailorly_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final UserRepository userRepository;
 
     @GetMapping("/api/test")
     public String test() {
         return "Tailorly Backend Running Successfully";
+    }
+
+    @GetMapping("/api/test/user")
+    public String createUser() {
+
+        User user = User.builder()
+                .name("Pranav")
+                .email("pranav@test.com")
+                .password("123456")
+                .build();
+
+        userRepository.save(user);
+
+        return "User Saved Successfully";
     }
 }
