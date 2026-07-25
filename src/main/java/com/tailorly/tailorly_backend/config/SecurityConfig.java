c5eead5299771d925de078c6e766a30234c7e502
pranavchavan17 <pranavc671@gmail.com>
2026-07-25 19:59:06 +0530

    chore: configure server port for Render

--- src/main/java/com/tailorly/tailorly_backend/config/SecurityConfig.java
+++ src/main/java/com/tailorly/tailorly_backend/config/SecurityConfig.java
@@ -36,21 +36,24 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                        "/api/v1/auth/**",
-                        "/api/v1/ats/**",
-                        "/api/test",
-                        "/actuator/**"
-                ).permitAll()
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
